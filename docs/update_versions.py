183cd65ffe9306c0ea9c666377e6e740fe2e5d0b
enmanuelmag <enmanuelmag@cardor.dev>
2022-08-09 13:41:30 +0200

    Enable command execution

--- docs/update_versions.py
+++ docs/update_versions.py
@@ -7,15 +7,16 @@ JSON_FILE = './docs/source/_static/versions.json'
 VERSIONS_FOLDER = '/usr/share/nginx/quasar/version'
 JSON_FILE_LOCAL = '/home/fmagalla/quasar/versions.json'
 
+PASS_USER = os.environ.get('PASS_USER')
+
 cp_local_command = f'cp {JSON_FILE} {JSON_FILE_LOCAL}'
 print(cp_local_command)
-#os.system(cp_command)
+os.system(cp_command)
 
 if __name__ == '__main__':
   for folder_version in os.listdir(VERSIONS_FOLDER):
-    print('Processing', folder_version)
+    print('Processing version: ', folder_version)
     
-    cp_nginx_command = f'cp {JSON_FILE_LOCAL} {VERSIONS_FOLDER}/{folder_version}/_static/versions.json'
-    print(cp_nginx_command)
-    #os.system(cp_nginx_command)
+    cp_nginx_command = f'echo {PASS_USER} | sudo -S cp {JSON_FILE_LOCAL} {VERSIONS_FOLDER}/{folder_version}/_static/versions.json'
+    os.system(cp_nginx_command)
   exit(0)
