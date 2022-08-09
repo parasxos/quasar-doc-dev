9147d772222f7931969a9832ac25fdba219602d2
enmanuelmag <enmanuelmag@cardor.dev>
2022-08-09 13:34:56 +0200

    Add local copy file

--- docs/update_versions.py
+++ docs/update_versions.py
@@ -5,12 +5,17 @@ import json
 
 JSON_FILE = './docs/source/_static/versions.json'
 VERSIONS_FOLDER = '/usr/share/nginx/quasar/version'
+JSON_FILE_LOCAL = '/home/fmagalla/quasar/versions.json'
 
+cp_local_command = f'cp {JSON_FILE} {JSON_FILE_LOCAL}'
+print(cp_local_command)
+#os.system(cp_command)
 
 if __name__ == '__main__':
   for folder_version in os.listdir(VERSIONS_FOLDER):
     print('Processing', folder_version)
-    cp_command = f'cp {JSON_FILE} {VERSIONS_FOLDER}/{folder_version}'
-    print(cp_command)
-    #os.system(cp_command)
+    
+    cp_nginx_command = f'cp {JSON_FILE_LOCAL} {VERSIONS_FOLDER}/{folder_version}/_static/versions.json'
+    print(cp_nginx_command)
+    #os.system(cp_nginx_command)
   exit(0)
